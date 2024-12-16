7d8cb062e2509944df6c1333c2e8440443324739
SirawitN <nine_sirawit@hotmail.com>
2024-12-16 12:07:26 +0700

    Add `server.py` for lab 2.2

---
+++ Lab2.2/server.py
@@ -0,0 +1,67 @@
+import socket, json
+
+def main():
+    host = ""
+    # TODO
+    # Specify your desired port
+    # =============================================
+    
+    
+    # =============================================
+    is_running = True
+
+    try:
+        # Create a TCP socket
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # TODO
+            # Complete the following block
+            # =============================================
+            
+            
+            # =============================================
+            s.settimeout(1.0)
+            print(f"[INFO] Server started, listening on port {port}")
+            
+            while is_running:
+                try:
+                    # TODO
+                    # Got a connection from a client
+                    # =============================================
+                    
+                    
+                    # =============================================
+                    print(f"[INFO] Connection accepted from {client_address}")
+                    
+                    with client_socket:
+                        print(f"[INFO] Connected to: {client_address[0]}:{client_address[1]}")
+                        
+                        # TODO
+                        # Receive data from the socket
+                        # =============================================
+                        
+                        
+                        # =============================================
+                        print("[INFO] Received message:", data)
+                        
+                        # TODO
+                        # Respond something to the client
+                        # =============================================
+                        
+                        
+                        # =============================================
+                        print(f"[INFO] Responded to Client {client_address[0]}:{client_address[1]}.")
+                        print(f"[INFO] Client {client_address[0]}:{client_address[1]} disconnected.")
+    
+                except socket.timeout:
+                    continue
+
+    except KeyboardInterrupt:
+        print("[INFO] Received shutdown signal (Ctrl+C). Stopping server...")
+        is_running = False
+    except Exception as e:
+        print(f"[ERROR] Exception in server loop: {e}")
+        
+    print("[INFO] Server shutdown complete.")
+            
+if __name__ == '__main__':
+    main()
